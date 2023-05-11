240c2324ee800f3e0215d6c3ec075571442332b3
unknown <aswindeva2408304@gmail.com>
2023-05-11 14:16:04 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,14 @@
+function displaydata(n){
+result.value+=n
+}
+function evaluateopn(){
+    result.value=eval(result.value)
+
+}
+
+function allclr(){
+    result.value=" "
+} 
+function bkspc(){
+    result.value=result.value.slice(0,-1)
+}
